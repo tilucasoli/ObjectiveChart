6ab0494f28fd03d59d1fe5a6ba5cc7973e6bdc08
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2021-07-21 10:16:51 -0300

    Upload iOS Framework base files

---
+++ ObjectiveChart/ObjectiveChart/ObjectiveChart.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveChart.h
+//  ObjectiveChart
+//
+//  Created by Lucas Oliveira on 21/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveChart.
+FOUNDATION_EXPORT double ObjectiveChartVersionNumber;
+
+//! Project version string for ObjectiveChart.
+FOUNDATION_EXPORT const unsigned char ObjectiveChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveChart/PublicHeader.h>
+
+
